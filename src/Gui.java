5572e27309416fabcdb95f140e629b6a22277194
pockerinos <kchen9988@me.com>
2020-01-26 04:01:58 -0500

    Added some stuff to test out GUI

--- src/Gui.java
+++ src/Gui.java
@@ -1,7 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
+
 
 public class Gui extends JFrame{
-    public static void main(String args[]){
+    private JLabel label;
+    private JButton button;
+    private JTextField textfield;
+
+    public Gui(){
+        setLayout(new FlowLayout());
+
+        label = new JLabel("Label");
+        add(label);
+
+        textfield = new JTextField(15);
+        add(textfield);
+
+        button = new JButton("Button");
+        add(button);
+
+    }
+
+    public static void main(String[] args){
         Gui window = new Gui();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(500,500);
