d1cf80d7c67e00a344ace43641c9033ddf5be596
pockerinos <kchen9988@me.com>
2020-06-03 16:04:47 -0400

    Created a different gui, commented out old one.

--- src/Gui.java
+++ src/Gui.java
@@ -1,18 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
 
-
+/*
 public class Gui extends JFrame{
-
     public Gui() {
         setLayout(new FlowLayout());
 
         ImageIcon image1 = new ImageIcon(getClass().getResource("image1.png"));
 
         JLabel imgLabel = new JLabel(image1);
+        imgLabel.setToolTipText("Welcome to the Stock Data Manager");
+        imgLabel.setSize(50,50);
         add(imgLabel);
 
-        JLabel label = new JLabel("Label");
+        JLabel label = new JLabel("Username");
+        label.setBounds(10,20,80,25);
         add(label);
 
         JTextField textfield = new JTextField(15);
@@ -30,5 +32,18 @@ public class Gui extends JFrame{
         window.setVisible(true);
         window.setTitle("Stock Data Manager");
         window.pack();
+    }
+}
+
+ */
+
+public class Gui {
+    public static void main (String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
     }
 }
