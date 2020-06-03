abc54588038ffb3170b550c83732ff3219935718
pockerinos <kchen9988@me.com>
2020-06-03 17:16:04 -0400

    Created a basic login gui.

---
+++ src/LoginGui.java
@@ -0,0 +1,104 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/*
+public class Gui extends JFrame{
+    public Gui() {
+        setLayout(new FlowLayout());
+
+        ImageIcon image1 = new ImageIcon(getClass().getResource("image1.png"));
+
+        JLabel imgLabel = new JLabel(image1);
+        imgLabel.setToolTipText("Welcome to the Stock Data Manager");
+        imgLabel.setSize(50,50);
+        add(imgLabel);
+
+        JLabel label = new JLabel("Username");
+        label.setBounds(10,20,80,25);
+        add(label);
+
+        JTextField textfield = new JTextField(15);
+        add(textfield);
+
+        JButton button = new JButton("Button");
+        add(button);
+
+    }
+
+    public static void main(String[] args){
+        Gui window = new Gui();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(500,500);
+        window.setVisible(true);
+        window.setTitle("Stock Data Manager");
+        window.pack();
+    }
+}
+
+ */
+
+public class LoginGui implements ActionListener {
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passLabel;
+    private static JTextField passText;
+    private static JButton login;
+    private static JLabel correct;
+
+
+
+    public static void main (String[] args) {
+        JPanel panel = new JPanel();
+
+        JFrame frame = new JFrame();
+        frame.setSize(400,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Login");
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("Username");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        passLabel = new JLabel("Password");
+        passLabel.setBounds(10, 50, 80, 25);
+        panel.add(passLabel);
+
+        passText = new JPasswordField(20);
+        passText.setBounds(100,50,165,25);
+        panel.add(passText);
+
+        login = new JButton("Login");
+        login.setBounds(10, 80, 80, 25);
+        login.addActionListener(new LoginGui());
+        panel.add(login);
+
+        correct = new JLabel("");
+        correct.setBounds(10,110,300,25);
+        panel.add(correct);
+
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String username = userText.getText();
+        String password = passText.getText();
+
+        if (username.equals("test") && password.equals("abc")) {
+            correct.setText("Logged in Successfully!");
+        }
+        else {
+            correct.setText("Incorrect username/password!");
+        }
+    }
+}
