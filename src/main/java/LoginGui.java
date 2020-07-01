8cf97043cdf439e12683d73ad89a04add2935975
pockerinos <kchen9988@me.com>
2020-07-01 15:20:13 -0400

    Cleaned up some code for LoginGui

--- src/main/java/LoginGui.java
+++ src/main/java/LoginGui.java
@@ -13,21 +13,16 @@ import java.awt.event.ActionListener;
 
 public class LoginGui implements ActionListener {
     private static JFrame frame;
-    private static JLabel userLabel;
     private static JTextField userText;
-    private static JLabel passLabel;
     private static JTextField passText;
-    private static JButton login;
-    private static JButton register;
     private static JLabel correct;
     private static MongoCollection<Document> usernames;
 
     public static MongoCollection<Document> mongoConnect() {
         MongoClient mongoClient = MongoClients.create();
         MongoDatabase database = mongoClient.getDatabase("SDMLoginDB");
-        MongoCollection<Document> collection = database.getCollection("Usernames");
 
-        return collection;
+        return database.getCollection("Usernames");
     }
 
     public static void main (String[] args) {
@@ -38,7 +33,6 @@ public class LoginGui implements ActionListener {
                 String username = userText.getText();
                 String password = passText.getText();
                 boolean loggedIn = false;
-                Block<Document> printBlock = document -> System.out.println(document.toJson());
                 System.out.println("Username: " + username);
                 System.out.println("Password: " + password);
                 if (username.isEmpty() && password.isEmpty()) {
@@ -77,7 +71,7 @@ public class LoginGui implements ActionListener {
 
         panel.setLayout(null);
 
-        userLabel = new JLabel("Username");
+        JLabel userLabel = new JLabel("Username");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
@@ -86,7 +80,7 @@ public class LoginGui implements ActionListener {
         userText.addActionListener(attemptLogin);
         panel.add(userText);
 
-        passLabel = new JLabel("Password");
+        JLabel passLabel = new JLabel("Password");
         passLabel.setBounds(10, 50, 80, 25);
         panel.add(passLabel);
 
@@ -95,12 +89,12 @@ public class LoginGui implements ActionListener {
         passText.addActionListener(attemptLogin);
         panel.add(passText);
 
-        login = new JButton("Login");
+        JButton login = new JButton("Login");
         login.setBounds(10, 80, 80, 25);
         login.addActionListener(new LoginGui());
         panel.add(login);
 
-        register = new JButton("Register");
+        JButton register = new JButton("Register");
         register.setBounds(100, 80, 90, 25);
         register.addActionListener(e -> {
             RegisterGui rg = new RegisterGui();
@@ -122,7 +116,6 @@ public class LoginGui implements ActionListener {
         String username = userText.getText();
         String password = passText.getText();
         boolean loggedIn = false;
-        Block<Document> printBlock = document -> System.out.println(document.toJson());
 
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
