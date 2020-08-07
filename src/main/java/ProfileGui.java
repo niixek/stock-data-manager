8f7f2d45e10383d0dcb6f76b34e2c5f10e5c399d
pockerinos <kchen9988@me.com>
2020-08-06 23:08:08 -0400

    Added two new classes for future use with the Welcome Gui

---
+++ src/main/java/ProfileGui.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ProfileGui implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/main/java/StockGui.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StockGui implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
